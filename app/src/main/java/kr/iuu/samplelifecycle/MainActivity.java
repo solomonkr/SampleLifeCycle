b8dbe57a42dc576f1b6ba6ed155e9449f0781fd1
raphael <>
2017-06-01 15:43:57 +0900

    Initial commit

---
+++ app/src/main/java/kr/iuu/samplelifecycle/MainActivity.java
@@ -0,0 +1,89 @@
+package kr.iuu.samplelifecycle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText nameInput;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"onStart 호출됨",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"onStop 호출됨",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"onDestroy 호출됨",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"onPause 호출됨",Toast.LENGTH_LONG).show();
+        saveState();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"onResume 호출됨",Toast.LENGTH_LONG).show();
+        restoreState();
+    }
+
+    protected void restoreState(){
+        SharedPreferences pref = getSharedPreferences("pref",Activity.MODE_PRIVATE);
+        if((pref != null) && (pref.contains("name")) ){
+            String name = pref.getString("name","");
+            nameInput.setText(name);
+        }
+    }
+
+    protected void saveState(){
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("name", nameInput.getText().toString());
+        editor.commit();
+    }
+
+    protected void clearMyprefs(){
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.clear();
+        editor.commit();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"onCreate 호출됨",Toast.LENGTH_LONG).show();
+
+        nameInput = (EditText) findViewById(R.id.nameinput);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
